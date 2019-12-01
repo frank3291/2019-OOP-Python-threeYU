817707c938d5b651457b93012f2b0ac5513a4485
Dongheon Yu <frank521@sasa.hs.kr>
2019-12-01 16:27:40 +0900

    파일명 수정

--- boss.py
+++ boss.py
@@ -26,6 +26,8 @@ END=pygame.image.load("end.png")
 END=pygame.transform.scale(END,(1500,600))
 badend=pygame.image.load("badend.jpg")
 badend=pygame.transform.scale(badend,(1500,600))
+
+global WINDOWEVENT, WINDOWHEIGHT, TEXTCOLOR, BACKGROUNDCOLOR, FPS, BADDIEMINSIZE, BADDIEMAXSIZE, BADDIEMINSPEED, BADDIEMAXSPEED, ADDNEWBADDIERATE, PLAYERMOVERATE, topScore
 WINDOWWIDTH = 1500
 WINDOWHEIGHT = 600
 TEXTCOLOR = (255, 255, 255)
@@ -37,6 +39,8 @@ BADDIEMINSPEED = 1
 BADDIEMAXSPEED = 8
 ADDNEWBADDIERATE = 6
 PLAYERMOVERATE = 5
+topScore = 0
+
 #
 global playerdata,playerImage
 Playerdata={'Name':'frank','rank':'A','count':10}
@@ -121,184 +125,189 @@ drawText('Press a key to start.', font, windowSurface, (WINDOWWIDTH / 2), (WINDO
 pygame.display.update()
 waitForPlayerToPressKey(False)
 
-topScore = 0
-while True:
-    cnt=0
-    baddies = []
-    hotspot={'rect':[0,0,0,0]}
-    flag=True
-    endgame=False
-    firsttime = True
-    score = 0
-    hp=1000
-    userhp=300
-    playerRect.topleft = (WINDOWWIDTH / 2, WINDOWHEIGHT - 50)
-    moveLeft = moveRight = moveUp = moveDown = False
-    reverseCheat = slowCheat = False
-    baddieAddCounter = 0
-    Levelcount=0
-    start_ticks = pygame.time.get_ticks()
+
+def main():
+    global WINDOWEVENT, WINDOWHEIGHT, TEXTCOLOR, BACKGROUNDCOLOR, FPS, BADDIEMINSIZE, BADDIEMAXSIZE, BADDIEMINSPEED, BADDIEMAXSPEED, ADDNEWBADDIERATE, PLAYERMOVERATE, topScore
     while True:
-        seconds = (pygame.time.get_ticks() - start_ticks) / 1000
-        score += 1
-        for event in pygame.event.get():
-            if event.type == KEYDOWN:
-                if event.key == K_SPACE:
-                    if hp <= 0:
-                        cnt += 1
-            if touchhotspot(hotspot):
-                if event.type==KEYDOWN:
-                    if event.key==K_SPACE:
-                        if hp<=0:
-                            cnt+=1
-                        hotspot['hp']-=2*playerdata['count']
-                        if hotspot['hp']<0:
-                            flag=True
-            if event.type == QUIT:
-                terminate()
+        cnt = 0
+        baddies = []
+        hotspot = {'rect': [0, 0, 0, 0]}
+        flag = True
+        endgame = False
+        firsttime = True
+        score = 0
+        hp = 1000
+        userhp = 300
+        playerRect.topleft = (WINDOWWIDTH / 2, WINDOWHEIGHT - 50)
+        moveLeft = moveRight = moveUp = moveDown = False
+        reverseCheat = slowCheat = False
+        baddieAddCounter = 0
+        Levelcount = 0
+        start_ticks = pygame.time.get_ticks()
+        while True:
+            seconds = (pygame.time.get_ticks() - start_ticks) / 1000
+            score += 1
+            for event in pygame.event.get():
+                if event.type == KEYDOWN:
+                    if event.key == K_SPACE:
+                        if hp <= 0:
+                            cnt += 1
+                if touchhotspot(hotspot):
+                    if event.type == KEYDOWN:
+                        if event.key == K_SPACE:
+                            if hp <= 0:
+                                cnt += 1
+                            hotspot['hp'] -= 2 * playerdata['count']
+                            if hotspot['hp'] < 0:
+                                flag = True
+                if event.type == QUIT:
+                    terminate()
 
-            if event.type == KEYDOWN:
-                if event.key == ord('z'):
-                    if playerdata['rank']=='A' or playerdata['rank']=='S':
-                        reverseCheat = True
-                if event.key == ord('x'):
-                    if playerdata['rank']=='S':
-                        slowCheat = True
-                if event.key == K_LEFT or event.key == ord('a'):
-                    moveRight = False
-                    moveLeft = True
-                if event.key == K_RIGHT or event.key == ord('d'):
-                    moveLeft = False
-                    moveRight = True
-                if event.key == K_UP or event.key == ord('w'):
-                    moveDown = False
-                    moveUp = True
-                if event.key == K_DOWN or event.key == ord('s'):
-                    moveUp = False
-                    moveDown = True
+                if event.type == KEYDOWN:
+                    if event.key == ord('z'):
+                        if playerdata['rank'] == 'A' or playerdata['rank'] == 'S':
+                            reverseCheat = True
+                    if event.key == ord('x'):
+                        if playerdata['rank'] == 'S':
+                            slowCheat = True
+                    if event.key == K_LEFT or event.key == ord('a'):
+                        moveRight = False
+                        moveLeft = True
+                    if event.key == K_RIGHT or event.key == ord('d'):
+                        moveLeft = False
+                        moveRight = True
+                    if event.key == K_UP or event.key == ord('w'):
+                        moveDown = False
+                        moveUp = True
+                    if event.key == K_DOWN or event.key == ord('s'):
+                        moveUp = False
+                        moveDown = True
 
-            if event.type == KEYUP:
-                if event.key == ord('z'):
-                    reverseCheat = False
-                    score = 0
-                if event.key == ord('x'):
-                    slowCheat = False
-                    score = 0
-                if event.key == K_ESCAPE:
+                if event.type == KEYUP:
+                    if event.key == ord('z'):
+                        reverseCheat = False
+                        score = 0
+                    if event.key == ord('x'):
+                        slowCheat = False
+                        score = 0
+                    if event.key == K_ESCAPE:
                         terminate()
 
-                if event.key == K_LEFT or event.key == ord('a'):
-                    moveLeft = False
-                if event.key == K_RIGHT or event.key == ord('d'):
-                    moveRight = False
-                if event.key == K_UP or event.key == ord('w'):
-                    moveUp = False
-                if event.key == K_DOWN or event.key == ord('s'):
-                    moveDown = False
+                    if event.key == K_LEFT or event.key == ord('a'):
+                        moveLeft = False
+                    if event.key == K_RIGHT or event.key == ord('d'):
+                        moveRight = False
+                    if event.key == K_UP or event.key == ord('w'):
+                        moveUp = False
+                    if event.key == K_DOWN or event.key == ord('s'):
+                        moveDown = False
 
-            #if event.type == MOUSEMOTION:
-                #playerRect.move_ip(event.pos[0] - playerRect.centerx, event.pos[1] - playerRect.centery)
+                # if event.type == MOUSEMOTION:
+                # playerRect.move_ip(event.pos[0] - playerRect.centerx, event.pos[1] - playerRect.centery)
 
-
-        if not reverseCheat and not slowCheat:
-            baddieAddCounter += 1
-        if baddieAddCounter == ADDNEWBADDIERATE:
-            baddieAddCounter = 0
-            baddieSize = random.randint(BADDIEMINSIZE, BADDIEMAXSIZE)
-            newBaddie = {'rect': pygame.Rect(random.randint(0, WINDOWWIDTH-baddieSize), 0 - baddieSize, baddieSize, baddieSize),
-                        'speed': random.randint(BADDIEMINSPEED, BADDIEMAXSPEED),
-                        'surface':pygame.transform.scale(baddieImage, (baddieSize, baddieSize)),
-                        }
-
-            baddies.append(newBaddie)
-
-        if moveLeft and playerRect.left > 0:
-            playerRect.move_ip(-1 * PLAYERMOVERATE, 0)
-        if moveRight and playerRect.right < WINDOWWIDTH:
-            playerRect.move_ip(PLAYERMOVERATE, 0)
-        if moveUp and playerRect.top > 0:
-            playerRect.move_ip(0, -1 * PLAYERMOVERATE)
-        if moveDown and playerRect.bottom < WINDOWHEIGHT:
-            playerRect.move_ip(0, PLAYERMOVERATE)
-
-        pygame.mouse.set_pos(playerRect.centerx, playerRect.centery)
-
-        for b in baddies:
             if not reverseCheat and not slowCheat:
-                b['rect'].move_ip(0, b['speed'])
-            elif reverseCheat:
-                b['rect'].move_ip(0, -5)
-            elif slowCheat:
-                b['rect'].move_ip(0, 1)
+                baddieAddCounter += 1
+            if baddieAddCounter == ADDNEWBADDIERATE:
+                baddieAddCounter = 0
+                baddieSize = random.randint(BADDIEMINSIZE, BADDIEMAXSIZE)
+                newBaddie = {
+                    'rect': pygame.Rect(random.randint(0, WINDOWWIDTH - baddieSize), 0 - baddieSize, baddieSize,
+                                        baddieSize),
+                    'speed': random.randint(BADDIEMINSPEED, BADDIEMAXSPEED),
+                    'surface': pygame.transform.scale(baddieImage, (baddieSize, baddieSize)),
+                    }
 
-        for b in baddies[:]:
-            if b['rect'].top > WINDOWHEIGHT:
-                baddies.remove(b)
+                baddies.append(newBaddie)
 
-        windowSurface.fill(BACKGROUNDCOLOR)
-        drawuserhp(userhp,windowSurface)
-        if flag:
-            if hp>0:
-                hp-=playerdata['count']*6
-            Levelcount+=1
-            BADDIEMINSPEED +=2
-            BADDIEMAXSPEED +=2
-            PLAYERMOVERATE +=1
-            flag=False
-            loc=makehotspot(hotspotImage, windowSurface)
-            x=loc[0]
-            y=loc[1]
-            hotspot={'rect':pygame.Rect(x, y, 100, 100),'hp':random.randint(Levelcount*40,Levelcount*80)}
-        windowSurface.blit(hotspotImage,(x,y))
-        drawText("%s"% (hotspot['hp']),font,windowSurface,x,y-20)
-        showbosshp(hp,windowSurface)
+            if moveLeft and playerRect.left > 0:
+                playerRect.move_ip(-1 * PLAYERMOVERATE, 0)
+            if moveRight and playerRect.right < WINDOWWIDTH:
+                playerRect.move_ip(PLAYERMOVERATE, 0)
+            if moveUp and playerRect.top > 0:
+                playerRect.move_ip(0, -1 * PLAYERMOVERATE)
+            if moveDown and playerRect.bottom < WINDOWHEIGHT:
+                playerRect.move_ip(0, PLAYERMOVERATE)
 
-        drawText('Score: %s' % (score), font, windowSurface, 10, 0)
-        drawText('Top Score: %s' % (topScore), font, windowSurface, 10, 40)
+            pygame.mouse.set_pos(playerRect.centerx, playerRect.centery)
 
-        windowSurface.blit(playerImage, playerRect)
+            for b in baddies:
+                if not reverseCheat and not slowCheat:
+                    b['rect'].move_ip(0, b['speed'])
+                elif reverseCheat:
+                    b['rect'].move_ip(0, -5)
+                elif slowCheat:
+                    b['rect'].move_ip(0, 1)
 
-        for b in baddies:
-            windowSurface.blit(b['surface'], b['rect'])
+            for b in baddies[:]:
+                if b['rect'].top > WINDOWHEIGHT:
+                    baddies.remove(b)
 
-        if playerHasHitBaddie(playerRect, baddies) :
-            userhp-=5*Levelcount//2
-            if userhp<=0:
-                break
-            if score > topScore:
-                topScore = score
-        if hp<=0:
-            limit=95-playerdata['count']
-            if firsttime:
-                firsttime=False
-                nowtime=seconds
-            timeleft=10-(seconds-nowtime)//1
-            text1=font.render('PRESS SPACE!!  MORE THAN %s!!!!!!'%(limit),True,(255,0,0))
-            text2=font.render('%s' % (cnt), True,(255,0,0))
-            text3=font.render('%s' % (timeleft),True,(255,0,0))
-            windowSurface.blit(text1,((WINDOWWIDTH/2)-200, (WINDOWHEIGHT/2)-50))
-            windowSurface.blit(text2,((WINDOWWIDTH/2)-150, (WINDOWHEIGHT/2)))
-            windowSurface.blit(text3,((WINDOWWIDTH/2)-150,(WINDOWHEIGHT/2)+50))
-            if timeleft<=0:
-                if cnt<limit:
-                    userhp=-10
+            windowSurface.fill(BACKGROUNDCOLOR)
+            drawuserhp(userhp, windowSurface)
+            if flag:
+                if hp > 0:
+                    hp -= playerdata['count'] * 6
+                Levelcount += 1
+                BADDIEMINSPEED += 2
+                BADDIEMAXSPEED += 2
+                PLAYERMOVERATE += 1
+                flag = False
+                loc = makehotspot(hotspotImage, windowSurface)
+                x = loc[0]
+                y = loc[1]
+                hotspot = {'rect': pygame.Rect(x, y, 100, 100), 'hp': random.randint(Levelcount * 40, Levelcount * 80)}
+            windowSurface.blit(hotspotImage, (x, y))
+            drawText("%s" % (hotspot['hp']), font, windowSurface, x, y - 20)
+            showbosshp(hp, windowSurface)
+
+            drawText('Score: %s' % (score), font, windowSurface, 10, 0)
+            drawText('Top Score: %s' % (topScore), font, windowSurface, 10, 40)
+
+            windowSurface.blit(playerImage, playerRect)
+
+            for b in baddies:
+                windowSurface.blit(b['surface'], b['rect'])
+
+            if playerHasHitBaddie(playerRect, baddies):
+                userhp -= 5 * Levelcount // 2
+                if userhp <= 0:
                     break
-                if cnt>=limit:
-                    break
+                if score > topScore:
+                    topScore = score
+            if hp <= 0:
+                limit = 95 - playerdata['count']
+                if firsttime:
+                    firsttime = False
+                    nowtime = seconds
+                timeleft = 10 - (seconds - nowtime) // 1
+                text1 = font.render('PRESS SPACE!!  MORE THAN %s!!!!!!' % (limit), True, (255, 0, 0))
+                text2 = font.render('%s' % (cnt), True, (255, 0, 0))
+                text3 = font.render('%s' % (timeleft), True, (255, 0, 0))
+                windowSurface.blit(text1, ((WINDOWWIDTH / 2) - 200, (WINDOWHEIGHT / 2) - 50))
+                windowSurface.blit(text2, ((WINDOWWIDTH / 2) - 150, (WINDOWHEIGHT / 2)))
+                windowSurface.blit(text3, ((WINDOWWIDTH / 2) - 150, (WINDOWHEIGHT / 2) + 50))
+                if timeleft <= 0:
+                    if cnt < limit:
+                        userhp = -10
+                        break
+                    if cnt >= limit:
+                        break
 
+            pygame.display.update()
+            mainClock.tick(FPS)
+
+        pygame.mixer.music.stop()
+        if userhp <= 0:
+            windowSurface.blit(badend, (0, 0))
+            drawText('You Lose!! ', font, windowSurface, 100, 450)
+            endgame = True
+        if hp <= 0 and userhp > 0:
+            windowSurface.blit(END, (0, 0))
+            drawText('You WIN!! THE END', font, windowSurface, 100, 450)
+            endgame = True
         pygame.display.update()
-        mainClock.tick(FPS)
+        pygame.time.wait(3000)
+        terminate()
 
-    pygame.mixer.music.stop()
-    if userhp<=0:
-        windowSurface.blit(badend,(0,0))
-        drawText('You Lose!! ', font, windowSurface, 100, 450)
-        endgame=True
-    if hp<=0 and userhp>0:
-        windowSurface.blit(END,(0,0))
-        drawText('You WIN!! THE END', font, windowSurface, 100, 450)
-        endgame=True
-    pygame.display.update()
-    pygame.time.wait(3000)
-    terminate()
+main()
 
